1aa47ff247277c793c49d4b9ffee1ea930a24a6b
zHuskyz <nvk01052004@gmail.com>
2024-10-23 09:38:28 +0700

    front-end 23/10

--- graphic.py
+++ graphic.py
@@ -7,67 +7,179 @@ Note:
   - #222a5c is the blue background
   - #38002c is the red background
 """
+chosenIndex = 0
 
-# Create object  
-root = Tk() 
+class tkinterApp(Tk):
+    # __init__ function for class tkinterApp 
+    def __init__(self, *args, **kwargs): 
+
+        # __init__ function for class Tk
+        Tk.__init__(self, *args, **kwargs)
+        self.title("Ares's Adventure")
+        self.geometry("1050x649")
+        self.resizable(False, False)
+        # creating a container
+        container = Frame(self)  
+        container.pack(side = "top", fill = "both", expand = True) 
   
-# Adjust size  
-root.title("Ares's adventure")
-root.geometry("1050x649") 
-root.resizable(0,0)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
   
-# Add image file 
-bg = PhotoImage(file = "Assets/bg.png") 
+        # initializing frames to an empty array
+        self.frames = {}  
   
-# Show image using label 
-label1 = Label( root, image = bg) 
-label1.place(x = 0, y = 0) 
-
-# Counter steps:
-counterStep = Label(root, text="Step: 0", fg="white", bg="#222a5c", padx=10, pady=10, width=5, height=1)
-counterStep.config(font=("Helvetica", 12, "bold"))
-counterStep.place(x = 20, y = 20)
-
-#pause button
-pauseImage = PhotoImage(file = "Assets/pause.png") 
-pauseButton = Button(root, bg="#222a5c", image=pauseImage, padx=0, pady=0)
-pauseButton.place(x = 982, y = 20)
-
-# Start button
-startImage = PhotoImage(file = "Assets/start.png") 
-startButton = Button(root, bg="#222a5c", image=startImage, padx=0, pady=0)
-startButton.place(x = 982, y = 82)
-
-# Restart button
-restartImage = PhotoImage(file = "Assets/restart.png") 
-restartButton = Button(root, bg="#222a5c", image=restartImage, padx=0, pady=0)
-restartButton.place(x = 982, y = 144)
-
-# BFS button
-bfsButton = Button(root, text="BFS", fg="white", bg="#38002c", padx=50, pady=1)
-bfsButton.configure(font=("Helvetica", 12, "bold"))
-bfsButton.place(x = 890, y = 601)
-
-# DFS button
-dfsButton = Button(root, text="DFS", fg="white", bg="#222a5c", padx=50, pady=1)
-dfsButton.configure(font=("Helvetica", 12, "bold"))
-dfsButton.place(x = 728, y = 601)
-
-# UCS button
-ucsButton = Button(root, text="UCS", fg="white", bg="#222a5c", padx=50, pady=1)
-ucsButton.configure(font=("Helvetica", 12, "bold"))
-ucsButton.place(x = 566, y = 601)
-
-# A* button
-astarButton = Button(root, text="A*", fg="white", bg="#222a5c", padx=58, pady=1)
-astarButton.configure(font=("Helvetica", 12, "bold"))
-astarButton.place(x = 405, y = 601)
-
-#  button
-
-# Create Frame 
-frame1 = Frame(root) 
-frame1.pack(pady = 20 ) 
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, GamePlay):
   
-# Execute tkinter 
-root.mainloop()
+            frame = F(container, self)
+  
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with 
+            # for loop
+            self.frames[F] = frame 
+  
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+  
+        self.show_frame(StartPage)
+  
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class StartPage(Frame):
+    def __init__(self, parent, controller): 
+        Frame.__init__(self, parent, width=1050, height=649)
+         
+        self.bg = PhotoImage(file = "Assets/bg.png") 
+
+        # Show image using label 
+        label1 = Label( self, image = self.bg) 
+        label1.place(x = 0, y = 0) 
+
+        # Title label
+        label = Label(self, text ="Ares's Adventure", font=("Helvetica", 40, "bold"), fg="white", bg="#222a5c")
+        label.place(relx=0.5, y= 100, anchor=CENTER)
+
+        
+        # Start button
+        self.startButton = Button(self, text="Start", fg="white", bg="#222a5c", padx=50, pady=1, command = lambda: self.start_btn_fn(controller))
+        self.startButton.configure(font=("Helvetica", 18, "bold"))
+        self.startButton.place(relx=0.5, y= 601, anchor=CENTER)
+
+        
+        # BFS button
+        self.bfsButton = Button(self, text="BFS", fg="white", bg="#222a5c", padx=50, pady=1, command=self.change_bfs_button_color)
+        self.bfsButton.configure(font=("Helvetica", 12, "bold"))
+        self.bfsButton.place(x = 705, y = 501)
+
+        # DFS button
+        self.dfsButton = Button(self, text="DFS", fg="white", bg="#222a5c", padx=50, pady=1,  command=self.change_dfs_button_color)
+        self.dfsButton.configure(font=("Helvetica", 12, "bold"))
+        self.dfsButton.place(x = 543, y = 501)
+
+        # UCS button
+        self.ucsButton = Button(self, text="UCS", fg="white", bg="#222a5c", padx=50, pady=1,  command=self.change_ucs_button_color)
+        self.ucsButton.configure(font=("Helvetica", 12, "bold"))
+        self.ucsButton.place(x = 381, y = 501)
+
+        # A* button
+        self.astarButton = Button(self, text="A*", fg="white", bg="#222a5c", padx=58, pady=1,  command=self.change_astar_button_color)
+        self.astarButton.configure(font=("Helvetica", 12, "bold"))
+        self.astarButton.place(x = 220, y = 501)
+
+        # Alert
+        self.alert = Label(self, text="Please choose an algorithm!", fg="red", bg="#222a5c")
+        self.alert.config(font=("Helvetica", 12, "bold"))
+
+    def start_btn_fn(self, controller):
+      if chosenIndex != 0:
+        controller.show_frame(GamePlay)
+      else:
+        self.alert.place(x = 415, y = 545)
+    
+    def change_bfs_button_color(self):
+        self.bfsButton.config(bg="#38002c")
+        self.dfsButton.config(bg="#222a5c")
+        self.ucsButton.config(bg="#222a5c")
+        self.astarButton.config(bg="#222a5c")
+        global chosenIndex
+        chosenIndex = 1
+        self.alert.destroy()
+    
+    def change_dfs_button_color(self):
+        self.bfsButton.config(bg="#222a5c")
+        self.dfsButton.config(bg="#38002c")
+        self.ucsButton.config(bg="#222a5c")
+        self.astarButton.config(bg="#222a5c")
+        global chosenIndex
+        chosenIndex = 2
+        self.alert.destroy()
+
+    def change_ucs_button_color(self):
+        self.bfsButton.config(bg="#222a5c")
+        self.dfsButton.config(bg="#222a5c")
+        self.ucsButton.config(bg="#38002c")
+        self.astarButton.config(bg="#222a5c")
+        global chosenIndex
+        chosenIndex = 3
+        self.alert.destroy()
+
+    def change_astar_button_color(self):
+        self.bfsButton.config(bg="#222a5c")
+        self.dfsButton.config(bg="#222a5c")
+        self.ucsButton.config(bg="#222a5c")
+        self.astarButton.config(bg="#38002c")
+        global chosenIndex
+        chosenIndex = 4
+        self.alert.destroy()
+
+
+class GamePlay(Frame):     
+    def __init__(self, parent, controller):
+      Frame.__init__(self, parent, width=1050, height=649 )
+      self.bg = PhotoImage(file = "Assets/bg.png") 
+
+      # Show image using label 
+      label1 = Label( self, image = self.bg) 
+      label1.place(x = 0, y = 0) 
+
+      # Counter steps:
+      self.counterStep = Label(self, text="Step: 0", fg="white", bg="#222a5c", padx=10, pady=10, width=5, height=1)
+      self.counterStep.config(font=("Helvetica", 12, "bold"))
+      self.counterStep.place(x = 20, y = 20)
+
+      #pause button
+      self.isPause = True
+      self.pauseImage = PhotoImage(file = "Assets/pause.png") 
+      self.startImage = PhotoImage(file = "Assets/start.png")  
+      self.pauseButton = Button(self, bg="#222a5c", image=self.startImage, padx=0, pady=0, command=self.change_pause_btn)
+      self.pauseButton.place(x = 982, y = 20)
+
+
+      # Start button
+      self.exitImage = PhotoImage(file = "Assets/exit.png")  
+      self.exitButton = Button(self, bg="#222a5c", image=self.exitImage, padx=0, pady=0, command = lambda : controller.show_frame(StartPage))
+      self.exitButton.place(x = 982, y = 82)
+
+      # Restart button
+      self.restartImage = PhotoImage(file = "Assets/restart.png") 
+      self.restartButton = Button(self, bg="#222a5c", image=self.restartImage, padx=0, pady=0)
+      self.restartButton.place(x = 982, y = 144)
+    
+    def change_pause_btn(self):
+      if self.isPause:
+        self.pauseButton.config(image=self.pauseImage)
+        self.isPause = False
+      else:
+        self.pauseButton.config(image=self.startImage)
+        self.isPause = True
+
+
+# Driver Code
+app = tkinterApp()
+app.mainloop()
+
